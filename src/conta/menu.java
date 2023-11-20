f02840ce5832a2f6eb4c19cc41f257141785dce9
Pedro-Vhiana <pedrohb.0222@gmail.com>
2023-11-20 14:54:11 -0300

    Projeto guia em andamento - Generation

--- src/conta/menu.java
+++ src/conta/menu.java
@@ -1,15 +1,24 @@
 package conta;
 
 import java.util.Scanner;
-
+import conta.model.Conta;
 import conta.util.Cores;
 
 public class menu {
     public static void main(String[] args) {
         
-        Scanner leia = new Scanner(System.in);
-		
-		int opcao;
+    	// Teste da Classe Conta
+    			Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+    			c1.visualizar();
+    			c1.sacar(12000.0f);
+    			c1.visualizar();
+    			c1.depositar(5000.0f);
+    			c1.visualizar();
+
+    			Scanner leia = new Scanner(System.in);
+
+    			int opcao;
+
 				
 		while(true) {
 
